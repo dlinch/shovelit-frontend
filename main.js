2f00ee08b40b81efb28e8fafab6ab17af868c210
dlinch <dlinch33@gmail.com>
2016-03-20 17:27:41 -0600

    index styling and adding content

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   console.log('jquery working');
 
-  $.get("http://localhost:3000/snow", function(data){
+  $.get("https://localhost:3000/snow", function(data){
     console.log(data);
     $('#snow').text(data);
   })
