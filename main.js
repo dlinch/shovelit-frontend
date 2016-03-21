2fb079f1684388176c28a9e18f251c040befc0c2
dlinch <dlinch33@gmail.com>
2016-03-21 15:06:15 -0600

    updated to production routes

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 $(document).ready(function(){
   console.log('jquery working');
 
-  $.get("https://localhost:3000/snow", function(data){
+  $.get("https://skyffel.herokuapp.com/snow", function(data){
     console.log(data);
     $('#snow').text(data);
   })
+
+  $('.login').click(function(){
+    console.log('login clicked');
+    $.ajax({
+      type: 'POST',
+      url: 'https://skyffel.herokuapp.com/auth/login',
+      data: {name: 'Derik', password: 'abc'},
+    }).done(function(result){
+      console.log(result.token)
+      localStorage.token = result.token;
+      getSecret();
+    })
+  })
+
+
+function getSecret() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://skyffel.herokuapp.com/secret',
+    headers: {
+      Authorization: 'Bearer' + localStorage.token
+    }
+  }).done(function(result){
+    console.log(result)
+  })
+}
+
+
+
+
+
+
+
+
 })
