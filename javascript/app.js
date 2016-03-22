c1c332284e7a959854f3a46f72eb8f084f7172f8
dlinch <dlinch33@gmail.com>
2016-03-21 18:42:23 -0600

    added angular route for pushing token to local storage

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,11 @@ var app = angular.module('skyffel', ['ngRoute'])
 
 app.config(function($routeProvider){
   $routeProvider.when('/', {
-    templateUrl: 'partials/home.html'
+    templateUrl: 'partials/home.html',
+    controller: 'HomeController'
+  })
+  .when('/token/:token', {
+    templateUrl: 'partials/token.html',
+    controller: 'TokenController'
   })
 })
---
+++ javascript/controllers.js
@@ -0,0 +1,11 @@
+app.controller('HomeController', function($scope, $http){
+
+})
+
+app.controller('TokenController', function($scope, $http, $routeParams, $location){
+
+  console.log($routeParams.token);
+  var token = $routeParams.token
+  localStorage.token = token;
+  $location.url('/')
+})
